25cd2d985854dd5c9b5bfdeede03358c89a86524
Dmitry <MitayLetsplay@gmail.com>
2023-08-02 18:27:14 +0300

    change log format in Post and Get methods

--- TestTasks/Controllers/DataController.cs
+++ TestTasks/Controllers/DataController.cs
@@ -30,16 +30,19 @@ namespace TestTasks.Controllers
         [HttpPost]
         public IActionResult Post(List<Dictionary<string, string>> data)
         {
-            _logger.Log(LogLevel.Information, $"[POST Data] Received data: {JsonConvert.SerializeObject(data)}");
+            _logger.Log(LogLevel.Information, $"[POST Data From: {Request.HttpContext.Connection.RemoteIpAddress}] Received data: {JsonConvert.SerializeObject(data)}", Request.HttpContext);
 
             try
             {
                 _appContext.AddFromDictList(data);
 
+                _logger.Log(LogLevel.Information, $"[POST Data To: {Request.HttpContext.Connection.RemoteIpAddress}] Status: 200 Ok");
+
                 return Ok();
             }
             catch(Exception ex)
             {
+                _logger.Log(LogLevel.Information, $"[POST Data To: {Request.HttpContext.Connection.RemoteIpAddress}] Status: 400 Bad Request", ex);
                 return BadRequest(ex);
             }            
         }
@@ -54,11 +57,13 @@ namespace TestTasks.Controllers
         [HttpGet]
         public IEnumerable<DataView> Get(int? code = null, string? value = null, int? id = null)
         {
-            _logger.Log(LogLevel.Information, $"[GET Data] Received get request. Filters: code = {code}, value = {value}, id = {id}");
+            _logger.Log(LogLevel.Information, $"[GET Data From: {Request.HttpContext.Connection.RemoteIpAddress}] Received request. Filters: code = {code}, value = {value}, id = {id}", Request.HttpContext);
 
             var data = _appContext.Get(code, value, id);
 
             var result = data.Select(x => _mapper.Map<DataView>(x));
+
+            _logger.Log(LogLevel.Information, $"[GET Data To: {Request.HttpContext.Connection.RemoteIpAddress}] Response: Filters: {JsonConvert.SerializeObject(result)}");
             return result;
         }
     }
